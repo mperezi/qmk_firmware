a31cec97d6f1b2a71004d270e07c5bcafd161556
mperezi <mperezibars@gmail.com>
2020-11-12 13:44:15 +0100

    Enable tap dance: send ñ on single tap, ~ on double tap

--- keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
@@ -1,12 +1,42 @@
 #include QMK_KEYBOARD_H
 
+// Tap Dance Definitions
+enum tapdance {
+    TD_TILDE = 0
+};
+
+void dance_tilde_finished(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		register_code16(KC_SCLN);
+	} else {
+		register_code16(KC_ALGR);
+		register_code16(KC_SCLN);
+	}
+}
+
+void dance_tilde_reset(qk_tap_dance_state_t *state, void *user_data) {
+	if (state->count == 1) {
+		unregister_code16(KC_SCLN);
+	} else {
+		unregister_code16(KC_SCLN);
+		unregister_code16(KC_ALGR);
+	}
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for ñ, twice for ~
+    [TD_TILDE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_tilde_finished, dance_tilde_reset),
+    /* [TD_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_GESC), */
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  MO(1),    KC_DEL,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
+    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     TD(TD_TILDE),  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
     KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RALT,  KC_RGUI,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
