e32798c49634c3a4a26c8c6a1ffdf806fc880adb
mperezi <mperezibars@gmail.com>
2020-11-16 00:37:02 +0100

    Tap dance i -> !

--- keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
@@ -11,6 +11,7 @@ enum layer_names {
 #define FN_AT     TD(TD_AT)
 #define FN_DOLLAR TD(TD_DOLLAR)
 #define FN_PIPE   TD(TD_PIPE)
+#define FN_EXCL   TD(TD_EXCL)
 
 #define L_CLCK    LSFT_T(KC_CAPS)
 #define R_CLCK    RSFT_T(KC_CAPS)
@@ -23,6 +24,7 @@ enum tapdance {
   TD_AT,
   TD_DOLLAR,
   TD_PIPE,
+  TD_EXCL,
 };
 
 void tap_dance_fn(qk_tap_dance_state_t *state, void *user_data) {
@@ -62,6 +64,14 @@ void tap_dance_fn(qk_tap_dance_state_t *state, void *user_data) {
         SEND_STRING(SS_ALGR(SS_TAP(X_1)));
       }
       break;
+    // Tap once for i, twice for !
+    case FN_EXCL:
+      if (count == 1) {
+        SEND_STRING(SS_TAP(X_I));
+      } else {
+        SEND_STRING(SS_RSFT(SS_TAP(X_1)));
+      }
+      break;
   }
   reset_tap_dance(state);
 }
@@ -70,7 +80,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_TILDE] = ACTION_TAP_DANCE_FN(tap_dance_fn),
   [TD_AT] = ACTION_TAP_DANCE_FN(tap_dance_fn),
   [TD_DOLLAR] = ACTION_TAP_DANCE_FN(tap_dance_fn),
-  [TD_PIPE] = ACTION_TAP_DANCE_FN(tap_dance_fn)
+  [TD_PIPE] = ACTION_TAP_DANCE_FN(tap_dance_fn),
+  [TD_EXCL] = ACTION_TAP_DANCE_FN(tap_dance_fn),
 };
 
 /* https://docs.qmk.fm/#/feature_led_indicators?id=example-led_update_user-implementation */
@@ -100,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  MO(1),    KC_DEL,
     ES_BLSH,  ES_1,     ES_2,     ES_3,     ES_4,     ES_5,     ES_6,     ES_7,     ES_8,     ES_9,     ES_0,     ES_QUOT,  ES_IEXL,  KC_DEL,   KC_BSPC,  KC_HOME,
-    KC_TAB,   ES_Q,     ES_W,     ES_E,     ES_R,     ES_T,     ES_Y,     ES_U,     ES_I,     ES_O,     ES_P,     ES_LBRC,  ES_RBRC,  ES_RCBR,            KC_PGUP,
+    KC_TAB,   ES_Q,     ES_W,     ES_E,     ES_R,     ES_T,     ES_Y,     ES_U,     FN_EXCL,  ES_O,     ES_P,     ES_LBRC,  ES_RBRC,  ES_RCBR,            KC_PGUP,
     KC_ESC,   FN_AT,    FN_DOLLAR,ES_D,     ES_F,     ES_G,     ES_H,     ES_J,     ES_K,     FN_PIPE,  FN_TILDE, ES_LCBR,                      KC_ENT,   KC_PGDN,
     L_CLCK,   KC_GRV,   ES_Z,     ES_X,     ES_C,     ES_V,     ES_B,     ES_N,     ES_M,     ES_COMM,  ES_DOT,   ES_MINS,  R_CLCK,             KC_UP,    KC_END,
     KC_LCTL,  KC_LALT,  KC_LGUI,                   KC_SPC,   LT(_CL, KC_SPC),   KC_SPC,                 KC_LGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
