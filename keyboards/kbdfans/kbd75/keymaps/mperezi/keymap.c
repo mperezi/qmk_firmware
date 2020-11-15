fcf7288fb319a4b205a25b7b6e771ae2b246d574
mperezi <mperezibars@gmail.com>
2020-11-15 18:03:42 +0100

    Use spanish keymap

--- keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/mperezi/keymap.c
@@ -1,15 +1,11 @@
 #include QMK_KEYBOARD_H
 
+#include "keymap_spanish.h"
+
 enum layer_names {
     _QW,
 	_CL
 };
-
-
-#define L_SQBRC   LALT(KC_LBRC)
-#define R_SQBRC   LALT(KC_RBRC)
-#define L_BRC     LALT(KC_QUOT)
-#define R_BRC     LALT(KC_BSLS)
 
 #define FN_TILDE  TD(TD_TILDE)
 #define FN_AT     TD(TD_AT)
@@ -47,35 +43,35 @@ void dance_tap_double_mod_reset(uint8_t count, uint16_t kc1, uint16_t kc2, uint1
 }
 
 void dance_tilde_finished(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_finished(state->count, KC_SCLN, KC_SCLN, KC_ALGR);
+	dance_tap_double_mod_finished(state->count, ES_NTIL, ES_NTIL, KC_ALGR);
 }
 
 void dance_tilde_reset(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_reset(state->count, KC_SCLN, KC_SCLN, KC_ALGR);
+	dance_tap_double_mod_reset(state->count, ES_NTIL, ES_NTIL, KC_ALGR);
 }
 
 void dance_at_finished(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_finished(state->count, KC_A, KC_2, KC_ALGR);
+	dance_tap_double_mod_finished(state->count, ES_A, ES_2, KC_ALGR);
 }
 
 void dance_at_reset(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_reset(state->count, KC_A, KC_2, KC_ALGR);
+	dance_tap_double_mod_reset(state->count, ES_A, ES_2, KC_ALGR);
 }
 
 void dance_dollar_finished(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_finished(state->count, KC_S, KC_4, KC_RSFT);
+	dance_tap_double_mod_finished(state->count, ES_S, ES_4, KC_RSFT);
 }
 
 void dance_dollar_reset(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_reset(state->count, KC_S, KC_4, KC_RSFT);
+	dance_tap_double_mod_reset(state->count, ES_S, ES_4, KC_RSFT);
 }
 
 void dance_pipe_finished(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_finished(state->count, KC_L, KC_1, KC_ALGR);
+	dance_tap_double_mod_finished(state->count, ES_L, ES_1, KC_ALGR);
 }
 
 void dance_pipe_reset(qk_tap_dance_state_t *state, void *user_data) {
-	dance_tap_double_mod_reset(state->count, KC_L, KC_1, KC_ALGR);
+	dance_tap_double_mod_reset(state->count, ES_L, ES_1, KC_ALGR);
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -112,11 +108,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: QWERTY */
   [_QW] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  MO(1),    KC_DEL,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     L_SQBRC,  R_SQBRC,  R_BRC,              KC_PGUP,
-    KC_ESC,   FN_AT,    FN_DOLLAR,KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     FN_PIPE,  FN_PIPE,  L_BRC,                        KC_ENT,   KC_PGDN,
-    L_CLCK,   MO(1),    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  R_CLCK,             KC_UP,    KC_END,
-    KC_LCTL,  KC_LALT,  KC_LGUI,                        KC_SPC,   LT(_CL, KC_SPC),   KC_SPC,                     KC_LGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_GRV,   ES_1,     ES_2,     ES_3,     ES_4,     ES_5,     ES_6,     ES_7,     ES_8,     ES_9,     ES_0,     ES_QUOT,  ES_IEXL,  KC_DEL,   KC_BSPC,  KC_HOME,
+    KC_TAB,   ES_Q,     ES_W,     ES_E,     ES_R,     ES_T,     ES_Y,     ES_U,     ES_I,     ES_O,     ES_P,     ES_LBRC,  ES_RBRC,  ES_RCBR,            KC_PGUP,
+    KC_ESC,   FN_AT,    FN_DOLLAR,ES_D,     ES_F,     ES_G,     ES_H,     ES_J,     ES_K,     FN_PIPE,  FN_TILDE, ES_LCBR,                      KC_ENT,   KC_PGDN,
+    L_CLCK,   MO(1),    ES_Z,     ES_X,     ES_C,     ES_V,     ES_B,     ES_N,     ES_M,     ES_COMM,  ES_DOT,   ES_MINS,  R_CLCK,             KC_UP,    KC_END,
+    KC_LCTL,  KC_LALT,  KC_LGUI,                   KC_SPC,   LT(_CL, KC_SPC),   KC_SPC,                 KC_LGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   /* 1: Custom */
